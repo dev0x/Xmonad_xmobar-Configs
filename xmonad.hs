6e552ff60a04fdf9962a5af48bb04345391bdf7a
dev0x <racine.matt@gmail.com>
2015-09-28 17:12:06 -0500

    add eclipse and re-organize pretty printing to be closer to logHook

--- xmonad.hs
+++ xmonad.hs
@@ -16,7 +16,6 @@ import XMonad.Hooks.FadeInactive
 import XMonad.Util.SpawnOnce
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.Scratchpad
---import XMonad.Util
 
 --layouts
 import XMonad.Layout.NoBorders
@@ -57,11 +56,12 @@ myBorderWidth = 1
 myManageHook = composeAll . concat $
   [ [ className =? "Chromium" --> doShift "web" ]
     , [ className =? "Gimp" --> doFloat]
-    , [ className =? "steam" --> doFloat]
+    , [ className =? "Eclipse" --> doFloat <+> doShift "code2"]
     , [ className =? "Steam" --> doFloat]
     , [ (className =? "Firefox" <&&> resource =? "Dialog") --> doFloat]
     , [ appName =? "crx_nckgahadagoaajjgafhacjanaoiihapd" --> (placeHook chatPlacement <+> doFloat)]
     , [ appName =? "crx_knipolnnllmklapflnccelgolnpehhpl" --> (placeHook chatPlacement <+> doFloat)]
+    , [ appName =? "crx_fahmaaghhglfmonjliepjlchgpgfmobi" --> (placeHook chatPlacement <+> doFloat)]
    ]
 
 chatPlacement :: Placement
@@ -73,7 +73,18 @@ chatPlacement = withGaps (0,16,1,0) (inBounds (smart (1,1)))
 myLogHook :: Handle -> X ()
 myLogHook h = dynamicLogWithPP $ customPP {ppOutput = hPutStrLn h}
 
---- Theme For Tabbed layout
+customPP :: PP
+customPP = defaultPP {
+    ppHidden = xmobarColor "#959595" ""
+    , ppCurrent = xmobarColor "#859900" "" . wrap "[" "]" 
+    , ppUrgent = xmobarColor "#FF0000" "" . wrap "*" "*"
+    , ppLayout = xmobarColor "#2AA198" "" 
+    , ppTitle = xmobarColor "#00FF00" "" . shorten 100
+    , ppSep = "<fc=#0033FF> | </fc>"
+}
+
+--------------------------------------------------------------------------------
+-- Theme For Tabbed layout
 myTheme = defaultTheme { decoHeight = 16
   , activeColor = "#000000"
   , activeBorderColor = "#A6C292"
@@ -105,17 +116,6 @@ myLayoutHook = onWorkspace "im" imLayout
     webL = avoidStruts $ (tabLayout ||| tiled ||| reflectHoriz tiled ||| Grid ||| magnifier (Tall 1 (3/100) (1/2)) ||| Full)
 
 --------------------------------------------------------------------------------
-customPP :: PP
-customPP = defaultPP {
-    ppHidden = xmobarColor "#959595" ""
-    , ppCurrent = xmobarColor "#859900" "" . wrap "[" "]" 
-    , ppUrgent = xmobarColor "#FF0000" "" . wrap "*" "*"
-    , ppLayout = xmobarColor "#2AA198" "" 
-    , ppTitle = xmobarColor "#00FF00" "" . shorten 100
-    , ppSep = "<fc=#0033FF> | </fc>"
-} 
-
---------------------------------------------------------------------------------
 --Run XMonad with the defaults
 main = do
   xmproc <- spawnPipe "xmobar -x 0 ~/.xmobarrc" 
@@ -130,7 +130,7 @@ main = do
     ,layoutHook = myLayoutHook
     ,modMask = myModMask
     ,keys = myKeys
-    ,startupHook = setWMName "LG3D" 
+    ,startupHook = setWMName "LG3D"
 } 
 
 
@@ -141,12 +141,13 @@ myKeys x = M.union (M.fromList (newKeys x)) (keys defaultConfig x)
 newKeys conf@(XConfig {XMonad.modMask = modm}) = [    
     ((modm, xK_p), spawn "dmenu_run -nb '#3F3F3F' -nf '#DCDCCC' -sb '#7F9F7F' -sf '#DCDCCC'")  --Uses a colorscheme with dmenu
     ,((modm, xK_f), spawn "urxvt -e xcalc")
-    ,((modm .|. shiftMask , xK_l), spawn "xtrlock -b")
-    ,((modm .|. shiftMask , xK_s), spawn "sudo pm-suspend")
-    ,((modm , xK_Return), spawn "urxvt")
     ,((modm , xK_m), spawn "chromium --app='https://mail.google.com'")
-    ,((modm , xK_0), nextWS)
     ,((0 , xK_Print), spawn "sleep 0.2; scrot -s")
+    ,((modm .|. shiftMask , xK_l), spawn "xscreensaver-command --lock")
+    ,((modm .|. shiftMask , xK_s), spawn "sudo pm-suspend")
+    ,((modm .|. shiftMask , xK_q), io (exitWith ExitSuccess))
+    ,((modm , xK_Return), spawn "urxvt")
+    ,((modm , xK_0), nextWS)   --move to next workspace
     ,((modm .|. controlMask , xK_equal), sendMessage MagnifyMore)
     ,((modm .|. controlMask , xK_minus), sendMessage MagnifyLess)
     ,((modm .|. controlMask , xK_o), sendMessage ToggleOff)
