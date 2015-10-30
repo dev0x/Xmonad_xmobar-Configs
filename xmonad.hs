808a7753ca18c037dc207fd14ec6c485e4ab3ac3
dev0x <racine.matt@gmail.com>
2015-10-30 15:20:09 -0500

    been a mashing of config changes from frankenpad to the changes that I haphazardly deploy at work

--- xmonad.hs
+++ xmonad.hs
@@ -1,20 +1,21 @@
--- Import Statements
+-- Import System  
 import XMonad
 import qualified Data.Map as M
-import Graphics.X11.Xlib
+import qualified XMonad.StackSet as W 
 import System.IO
 import System.Exit
+import Control.Monad
 
+-- actions
+import XMonad.Actions.CycleWS
+import XMonad.Actions.DynamicWorkspaces
+import XMonad.Actions.CopyWindow
 -- hooks
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers()
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.SetWMName
 import XMonad.Hooks.Place
-
--- utils
-import XMonad.Util.SpawnOnce
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.Scratchpad
 
 --layouts
 import XMonad.Layout.NoBorders
@@ -25,44 +26,69 @@ import XMonad.Layout.Tabbed
 import XMonad.Layout.PerWorkspace(onWorkspace)
 import XMonad.Layout.Grid
 import XMonad.Layout.ThreeColumns
-import XMonad.Layout.SimplestFloat
-import XMonad.Layout.Spacing
---actions
-import XMonad.Actions.CycleWS
+import XMonad.Layout.Magnifier
 
 
--- Data.Ratio due to the IM layout
-import Data.Ratio ((%))
+-- prompt
+import XMonad.Prompt
+
+-- utils
+import XMonad.Util.SpawnOnce
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.Dmenu
+
+
+-- Data.Ratio -  IM layout
+import Data.Ratio((%))
 
 --------------------------------------------------------------------------------
+-- self defined functions
+--------------------------------------------------------------------------------
+-- Quit with warning
+quitWithWarning :: X ()    
+quitWithWarning = do
+    let message = "confirm quit"
+    s <- dmenu [message]
+    when (s == "y") (io exitSuccess)
+--------------------------------------------------------------------------------
+-- self defined variables
+--------------------------------------------------------------------------------
 --Define Terminal
-myTerminal :: String
+myTerminal  :: String
 myTerminal = "urxvt"
+
+--Define MyModMask
+myModMask   :: KeyMask
+myModMask = mod1Mask 
 
 --------------------------------------------------------------------------------
 --Workspaces
 myWorkspaces :: [WorkspaceId]
 myWorkspaces = ["tmux","web","web2","code","code1","code2","im","term","term1","moo!and!oink"]
 
---------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 -- Make the bordercolor different here because well...  this is where it is defined.  BAM spice-weasel!
+myNormalBorderColor :: String
 myNormalBorderColor = "#808080"
+myFocusedBorderColor :: String
 myFocusedBorderColor = "#009900"
+
+--Define to def...
+myXPConfig :: XPConfig
+myXPConfig = defaultXPConfig
 
 --------------------------------------------------------------------------------
 -- MangeDocks --> ManageHook 
+myManageHook :: ManageHook 
 myManageHook = composeAll . concat $
    [ [ className =? "Chromium" --> doShift "web" ]
-   , [ className =? "Firefox-bin" --> doShift "web2"]
-   , [ appName =? "crx_nckgahadagoaajjgafhacjanaoiihapd"    --> (placeHook chatPlacement <+> doShift "im" <+> doFloat) ] -- hangouts
-   , [ appName =? "crx_knipolnnllmklapflnccelgolnpehhpl"    --> (placeHook chatPlacement <+> doShift "im" <+> doFloat) ] -- hangouts 
-   , [ appName =? "crx_fahmaaghhglfmonjliepjlchgpgfmobi"    --> (placeHook gmusicPlacement <+> doShift "term1" <+> doFloat) ] -- gmusic player 
-   , [ (className =? "Firefox" <&&> resource =? "Dialog") --> doFloat]
+       , [ className =? "Firefox" --> doShift "web2"]
+       , [ className =? "Eclipse" --> doShift "code2" <+> doFloat]
+       , [ appName =? "crx_nckgahadagoaajjgafhacjanaoiihapd"    --> (placeHook chatPlacement <+> doShift "im" <+> doFloat) ] 
+       , [ appName =? "crx_knipolnnllmklapflnccelgolnpehhpl"    --> (placeHook chatPlacement <+> doFloat) ] 
+       , [ appName =? "crx_fahmaaghhglfmonjliepjlchgpgfmobi"    --> (placeHook gmusicPlacement <+> doShift "term1" <+> doFloat) ] --gmusic 
+       , [ (className =? "Firefox" <&&> resource =? "Dialog") --> doFloat]
    ]
-   -- in a composeAll hook, you'd use: fmap ("VLC" `isInfixOf`) title --> doFloat
-  where myFloatsC = ["Hangouts", "donkey-doodle"]
-        myMatchAnywhereFloatsC = ["Google","hangouts"]
-        myMatchAnywhereFloatsT = ["VLC"] -- this one is silly for only one string!
 
 chatPlacement :: Placement
 chatPlacement = withGaps(0,16,1,0) (inBounds (smart(0,1)))
@@ -72,44 +98,7 @@ gmusicPlacement = withGaps(0,16,1,0) (inBounds (smart(0,1)))
 --------------------------------------------------------------------------------
 --logHook
 myLogHook :: Handle -> X ()
-myLogHook h = dynamicLogWithPP $ dev0xPP {ppOutput = hPutStrLn h}
-
---- Theme For Tabbed layout
-myTheme = defaultTheme { decoHeight = 16
-            , activeColor = "#000000"
-            , activeBorderColor = "#A6C292"
-            , activeTextColor = "#CEFFAC"
-            , inactiveColor = "#000000"
-            , inactiveBorderColor = "#7C7C7C"
-            , inactiveTextColor = "#EEEEEE"
-            }
---------------------------------------------------------------------------------
-myLayoutHook  =  onWorkspace "im" imLayout 
-         $ onWorkspace "web" webL
-         $ onWorkspace "web2" webL
-         $ standardLayouts 
-    where
-    threeLayout = ThreeCol 1 (3/100) (1/2)
-    standardLayouts =  avoidStruts $ (tiled ||| reflectTiled ||| Mirror tiled ||| Grid ||| Full ||| threeLayout)
- 
-    --Layouts
-    tiled      = smartBorders (ResizableTall 1 (2/100) (1/2) [])
-    reflectTiled  = (reflectHoriz tiled)
-    tabLayout    = (tabbed shrinkText myTheme)
-    full      = noBorders Full
- 
-    --Im Layout
-    imLayout = withIM (1/10) (Role "roster") (standardLayouts)
-     
-    --Web Layout
-    webL = avoidStruts $ (tabLayout  ||| tiled ||| reflectHoriz tiled ||| Grid ||| Full)
-    gimpLayout = smartBorders(avoidStruts(ThreeColMid 1 (3/100) (3/4)))
-     
---------------------------------------------------------------------------------
-myStartupHook :: X ()
-myStartupHook = do
-        setWMName "LG3D" 
-        spawnOnce "xmobar -x 1 ~/.xmobarrc2"
+myLogHook h = dynamicLogWithPP $ dev0xPP {ppOutput = hPutStrLn h} 
 
 dev0xPP :: PP
 dev0xPP = defaultPP {
@@ -121,32 +110,100 @@ dev0xPP = defaultPP {
       , ppTitle = xmobarColor "#00FF00" "" . shorten 80
       , ppSep = "<fc=#0033FF> | </fc>"
     } 
- 
+
+--- Theme For Tabbed layout
+myTabTheme :: Theme 
+myTabTheme = defaultTheme { decoHeight = 16
+            , activeColor = "#000000"
+            , activeBorderColor = "#A6C292"
+            , activeTextColor = "#CEFFAC"
+            , inactiveColor = "#000000"
+            , inactiveBorderColor = "#7C7C7C"
+            , inactiveTextColor = "#EEEEEE"
+            }
+
 --------------------------------------------------------------------------------
+-- Layout Specification - this might be a little messy
+--------------------------------------------------------------------------------
+--  This Type Signature is a bit crazy - a bunch of expletives summarize trying to write this crap out lol
+{-
+myLayoutHook :: onWorkspace() 
+-}
+myLayoutHook = onWorkspace "im" imLayout 
+             $ onWorkspace "web" webLayout
+             $ onWorkspace "web1" webLayout
+             $ standardLayouts 
+    where
+    standardLayouts     = avoidStruts $ (tiled ||| magLayout ||| reflectTiled ||| Grid ||| Full ||| threeLayout)
+    tiled               = smartBorders (ResizableTall 1 (2/100) (1/2) [])
+    reflectTiled        = (reflectHoriz tiled)
+
+    --magnifier Layout
+    magLayout           = magnifier (Tall 1 (3/100) (1/2))
+
+    --tab Layout
+    tabLayout           = (tabbed shrinkText myTabTheme)
+
+    --Three Columnar window layout
+    threeLayout         = ThreeCol 1 (3/100) (1/2)
+
+    --Im Layout
+    imLayout            = withIM (1%10) (Role "roster") (standardLayouts)
+     
+    --Web Layout
+    webLayout           = avoidStruts $ (tabLayout ||| magLayout ||| reflectTiled ||| Grid ||| Full ||| threeLayout)
+     
+-------------------------------------------------------------------------------
+myStartupHook :: X ()
+myStartupHook = do
+        setWMName "LG3D" 
+        spawnOnce "xmobar -x 1 ~/.xmobarrc2"
+-------------------------------------------------------------------------------
 --Run XMonad with the defaults
+main :: IO ()
 main = do
-  xmproc <- spawnPipe "xmobar -x 0 ~/.xmobarrc" 
-  xmonad  $ defaultConfig { 
-  terminal = myTerminal
-  ,workspaces = myWorkspaces
-    ,manageHook = myManageHook
-  ,normalBorderColor = myNormalBorderColor
-  ,focusedBorderColor = myFocusedBorderColor
-  ,logHook = myLogHook xmproc
-  ,layoutHook = myLayoutHook
-  ,keys = myKeys
-  ,startupHook = myStartupHook 
+  xmobarProc <- spawnPipe "xmobar -x 0 ~/.xmobarrc" 
+  xmonad $ defaultConfig { 
+      focusedBorderColor = myFocusedBorderColor
+      ,normalBorderColor = myNormalBorderColor
+      ,logHook = myLogHook xmobarProc
+      ,layoutHook = myLayoutHook
+      ,manageHook = myManageHook
+      ,modMask = myModMask
+      ,keys = myKeys 
+      ,startupHook = myStartupHook
+      ,terminal = myTerminal
+      ,workspaces = myWorkspaces
   } 
 
-myKeys x = M.union (M.fromList (newKeys x)) (keys defaultConfig x)
---------------------------------------------------------------------------------
-newKeys conf@(XConfig {XMonad.modMask = mod4Mask}) = [    
-    ((mod4Mask, xK_p), spawn "dmenu_run -nb '#3F3F3F' -nf '#DCDCCC' -sb '#7F9F7F' -sf '#DCDCCC'")  --Uses a colorscheme with dmenu
-    ,((mod4Mask, xK_f), spawn "urxvt -e xcalc")
-    ,((mod4Mask.|.shiftMask, xK_l), spawn "xscreensaver-command --lock")
-    ,((mod4Mask, xK_Return), spawn "urxvt")
-    ,((mod4Mask, xK_m), spawn "chromium-browser --app='https://mail.google.com'")
-    ,((0, xK_Print), spawn "sleep 0.2; scrot -s")
-    ,((mod4Mask, xK_0), nextWS)
-  ]
+-------------------------------------------------------------------------------
+--Keys
+myKeys   :: XConfig Layout -> M.Map (KeyMask, KeySym) (X())
+myKeys x = M.union(M.fromList (newKeys x)) (keys defaultConfig x)
+-- newKeys is self defined configuration to which then gets merged into myKeys to then be pulled into xmonad conf
+newKeys  :: XConfig Layout -> [((KeyMask, KeySym), X())]
+newKeys (XConfig {XMonad.modMask = modm}) =
+    [    
+        ((modm                   ,xK_p)          ,spawn "dmenu_run -nb '#3F3F3F' -nf '#DCDCCC' -sb '#7F9F7F' -sf '#DCDCCC'")  --Uses colorscheme with dmenu
+        ,((modm                  ,xK_f)          ,spawn "urxvt -e xcalc")
+        ,((modm                  ,xK_Return)     ,spawn "urxvt")
+        ,((modm                  ,xK_m)          ,spawn "chromium-browser --app='https://mail.google.com'")
+        ,((0                     ,xK_Print)      ,spawn "sleep 0.5; scrot -s")
+        ,((modm                  ,xK_0)          ,nextWS)
+        ,((modm.|.shiftMask      ,xK_0)          ,prevWS)
+        ,((modm.|.shiftMask      ,xK_q)          ,quitWithWarning)
+        ,((modm.|.shiftMask      ,xK_l)          ,spawn "xscreensaver-command --lock")
+        ,((modm.|.shiftMask      ,xK_BackSpace)  ,removeWorkspace)
+        ,((modm.|.shiftMask      ,xK_v)          ,selectWorkspace myXPConfig)
+
+        ,((modm                  ,xK_m)          ,withWorkspace myXPConfig (windows . W.shift))
+        ,((modm.|.shiftMask      ,xK_m)          ,withWorkspace myXPConfig (windows . copy))
+
+        ,((modm.|.shiftMask      ,xK_r)          ,renameWorkspace myXPConfig)
+        ,((modm                  ,xK_a)          ,addWorkspacePrompt myXPConfig)
+    ]
+    ++
+    zip (zip (repeat (modm)) [xK_1..xK_9]) (map (withNthWorkspace W.greedyView) [0..])
+    ++
+    zip (zip (repeat (modm .|. shiftMask)) [xK_1..xK_9]) (map (withNthWorkspace W.shift) [0..])
 --------------------------------------------------------------------------------
